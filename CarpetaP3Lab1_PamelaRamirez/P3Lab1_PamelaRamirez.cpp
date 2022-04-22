571f9d776e6253aaf0a76ff4d0c1839d941dfdaa
pamelagiselle8 <pamelaramirez@unitec.edu>
2022-04-22 14:28:51 -0600

    hola

---
+++ CarpetaP3Lab1_PamelaRamirez/P3Lab1_PamelaRamirez.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int incognita;
+    cout << "---Teorema de Pitagoras--- \n"
+    << "Seleccione incognita A/B/C: ";
+    cin >> incognita;
+    if (incognita == 1) {
+        cout << "---Teorema de Pitagoras--- \n";
+    }
+    else if (incognita == 2) {
+        cout << "---Teorema de Pitagoras--- \n";
+    }
+    else if (incognita == 3) {
+        cout << "---Teorema de Pitagoras--- \n";
+    }
+    else{
+        cout << "Opcion invalida \n";
+    }
+    return 0;
+}
